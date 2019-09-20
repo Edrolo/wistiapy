b07969c666931ff9626be8aa013235958cab0901
Matt Fisher <m@ttfisher.com>
2019-09-20 17:08:14 +1000

    Factory function to assist configuration

--- wistia/__init__.py
+++ wistia/__init__.py
@@ -2,3 +2,4 @@ from .__version__ import __version__
 
 from .api import WistiaClient
 from .dummy import DummyWistiaClient
+from .helpers import get_wistia_client
---
+++ wistia/helpers.py
@@ -0,0 +1,34 @@
+from os import environ
+
+from wistia.api import WistiaClient
+from wistia.dummy import DummyWistiaClient
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def get_wistia_client(password: str = None, client_cls=None) -> WistiaClient:
+    # Calling with password='' will get you a DummyWistiaClient
+    try:
+        # If we're running Django, pull WISTIA_CLIENT_CLASS from settings
+        from django.conf import settings
+        if client_cls is None:
+            client_class_name = getattr(settings, 'WISTIA_CLIENT_CLASS', 'WistiaClient')
+            client_cls = globals()[client_class_name]
+
+        password = password or getattr(settings, 'WISTIA_API_PASSWORD', None)
+    except ImportError:
+        pass
+
+    if password is None:
+        password = environ.get('WISTIA_API_PASSWORD')
+
+    if client_cls is None:
+        if password is None or password == '':
+            log.info('No api password found for wistia client - using dummy client')
+            client_cls = DummyWistiaClient
+        else:
+            client_cls = WistiaClient
+
+    return client_cls(api_password=password)
