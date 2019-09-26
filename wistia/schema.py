e2b89b2d6433566de72efd82d6a5eba35fd805e1
Matt Fisher <m@ttfisher.com>
2019-09-26 16:16:12 +1000

    Flesh out schemas

--- wistia/schema.py
+++ wistia/schema.py
@@ -12,14 +12,16 @@ class Asset:
         """(optional) The height of this specific asset, if applicable."""
         self.height = dict["height"]
         """The size of the asset file that's referenced by url, measured in bytes."""
-        self.fileSize = dict["fileSize"]
+        self.file_size = dict["fileSize"]
         """The asset's content type."""
-        self.contentType = dict["contentType"]
+        self.content_type = dict["contentType"]
         """
         The internal type of the asset, describing how the asset should be used.
-        Valid values are "OriginalFile", "FlashVideoFile", "Mp4VideoFile",
-        "IPhoneVideoFile", "StillImageFile", "SwfFile", "Mp3AudioFile",
-        and "LargeImageFile"
+        Valid values are "OriginalFile", 
+        "FlashVideoFile", "MdFlashVideoFile", "HdFlashVideoFile", 
+        "Mp4VideoFile", "MdMp4VideoFile, "HdMp4VideoFile", 
+        "IPhoneVideoFile", "StillImageFile", "SwfFile", 
+        "Mp3AudioFile", and "LargeImageFile".
         """
         self.type = dict["type"]
 
@@ -35,18 +37,31 @@ class Media:
         self.id = media_dict["id"]
         """The display name of the media."""
         self.name = media_dict["name"]
+        """A unique alphanumeric identifier for this media."""
+        self.hashed_id = media_dict["hashed_id"]
+        """	
+        A description for the media which usually appears near the top of the
+        sidebar on the media’s page.
+        """
+        self.description = media_dict["description"]
         """
         An object representing information about the project in which the media
         resides. It has 2 fields: the numeric id of the project, and the name
         of the project.
         """
-        self.project = media_dict["project"]
+        project = media_dict.get("project")
+        self.project = ProjectReference(project) if project else None
         """
         A string representing what type of media this is. Valid values are "Video",
         "Image", "Audio", "Swf", "MicrosoftOfficeDocument", "PdfDocument", or
         "UnknownType".
         """
         self.type = media_dict["type"]
+        """
+        Post upload processing status. There are four statuses: 
+        "queued", "processing", "ready", and "failed".
+        """
+        self.status = media_dict["status"]
         """
         (optional) After a file has been uploaded to Wistia, it needs to be
         processed before it is available for online viewing. This field is
@@ -60,10 +75,9 @@ class Media:
         """
         self.section = media_dict.get("section")
         """
-        An object representing the thumbnail for this media. The attributes are
-        URL, width, and height.
+        An object representing the thumbnail for this media.
         """
-        self.thumbnail = media_dict["thumbnail"]
+        self.thumbnail = Thumbnail(media_dict["thumbnail"])
         """
         (optional) For Audio or Video files, this field specifies the length
         (in seconds). For Document files, this field specifies the number of
@@ -80,14 +94,29 @@ class Media:
         for a description the fields in each asset object.
         """
         self.assets = [Asset(asset_dict) for asset_dict in media_dict["assets"]]
-        """
+        r"""
+        DEPRECATED
         The HTML code that would be used for embedding the media into a web page.
         Please note that in JSON format, all quotes are escaped with a
-        backslash (\\) character. In XML, angle brackets (< and >) and
+        backslash (\) character. In XML, angle brackets (< and >) and
         ampersands (&) are converted to their equivalent XML entities
         ("&lt;", "&gt;", and "&amp;" respectively) to prevent XML parser errors.
         """
-        self.embedCode = media_dict["embedCode"]
+        self.embed_code = media_dict["embedCode"]
+
+
+class Thumbnail:
+    def __init__(self, thumbnail_dict):
+        self.URL = thumbnail_dict["URL"]
+        self.width = thumbnail_dict["width"]
+        self.height = thumbnail_dict["height"]
+
+
+class ProjectReference:
+    def __init__(self, project_reference_dict):
+        self.id = project_reference_dict["id"]
+        self.name = project_reference_dict["name"]
+        self.hashed_id = project_reference_dict["hashed_id"]
 
 
 class Project:
@@ -99,7 +128,7 @@ class Project:
         """The project's display name."""
         self.name = project_dict["name"]
         """The number of different medias that have been uploaded to the project."""
-        self.mediaCount = project_dict["mediaCount"]
+        self.media_count = project_dict["mediaCount"]
         """The date that the project was originally created."""
         self.created = project_dict["created"]
         """The date that the project was last updated"""
@@ -108,17 +137,17 @@ class Project:
         A private hashed id, uniquely identifying the project within the
         system. Used for playlists and RSS feeds.
         """
-        self.hashedId = project_dict["hashedId"]
+        self.hashed_id = project_dict["hashedId"]
         """
         A boolean indicating whether or not anonymous uploads are enabled for the
         project.
         """
-        self.anonymousCanUpload = project_dict["anonymousCanUpload"]
+        self.anonymous_can_upload = project_dict["anonymousCanUpload"]
         """
         A boolean indicating whether or not anonymous downloads are enabled for
         this project.
         """
-        self.anonymousCanDownload = project_dict["anonymousCanDownload"]
+        self.anonymous_can_download = project_dict["anonymousCanDownload"]
         """
         A boolean indicating whether the project is available for public
         (anonymous) viewing.
@@ -128,7 +157,7 @@ class Project:
         If the project is public, this field contains a string representing the
         ID used for referencing the project in public URLs.
         """
-        self.publicId = project_dict["publicId"]
+        self.public_id = project_dict["publicId"]
         """
         In the project show, you can get a list of the media associated with
         a project.
