d51f323749811fc67d127bfc9db4f6aa757cc353
Matt Fisher <m@ttfisher.com>
2019-09-30 16:41:18 +1000

    Export schemas from wistia package

--- wistia/__init__.py
+++ wistia/__init__.py
@@ -1,5 +1,12 @@
 from .__version__ import __version__
-
 from .client import WistiaClient
 from .dummy import DummyWistiaClient
 from .helpers import get_wistia_client
+from .schema import (
+    Asset,
+    CaptionTrack,
+    Media,
+    Project,
+    ProjectReference,
+    Thumbnail,
+)
