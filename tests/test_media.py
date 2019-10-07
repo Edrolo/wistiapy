7acaae84d32de215aa6012a541832eade37a2edb
Matt Fisher <m@ttfisher.com>
2019-10-07 21:29:25 +1100

    Tests

--- tests/test_media.py
+++ tests/test_media.py
@@ -1,9 +1,13 @@
+import base64
 import logging
 from os import path
 
 import pytest
 import json
 
+import responses
+
+from wistia import WistiaClient
 from wistia.schema import Media
 
 log = logging.getLogger("test_wistia")
@@ -23,4 +27,44 @@ def test_media_parsing(media_list):
 
 def test_direct_instantiation_of_schema_is_deep(media_list):
     medias = [Media(media_data) for media_data in media_list]
-    assert medias[0].thumbnail.url == media_list[0]['thumbnail']['url']
+    assert medias[0].thumbnail.url == media_list[0]["thumbnail"]["url"]
+
+
+def generate_http_basic_auth_string(username, password):
+    encoded_string = base64.b64encode(f"{username}:{password}".encode("utf-8")).decode(
+        "ascii"
+    )
+    return f"Basic {encoded_string}"
+
+
+@responses.activate
+def test_authentication_set_correctly_in_header():
+    # Given
+    password = "let-me-in"
+    client = WistiaClient(api_password=password)
+    expected_url = f"https://api.wistia.com/v1/medias.json"
+    responses.add(responses.GET, url=expected_url, json=[], status=200)
+
+    # When
+    client.list_medias()
+
+    # Then
+    expected_auth_header = generate_http_basic_auth_string("api", password)
+    request, response = responses.calls[0]
+    observed_auth_header = request.headers["Authorization"]
+    assert observed_auth_header == expected_auth_header
+
+
+@responses.activate
+def test_purchase_captions_hits_correct_endpoint():
+    media_hashed_id = "12345"
+    expected_url = (
+        f"https://api.wistia.com/v1/medias/{media_hashed_id}/captions/purchase.json"
+    )
+    responses.add(responses.POST, url=expected_url, json={}, status=200)
+
+    WistiaClient().purchase_captions(media_hashed_id)
+    request, response = responses.calls[0]
+    assert request.url == expected_url
+    assert request.method == "POST"
+    assert request.body is None
