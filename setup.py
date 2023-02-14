b519fb2521f7900bb325f1d7d02184b03d74a61f
Matt Fisher <m@ttfisher.com>
2023-02-14 17:26:17 +1100

    Bump schematics requirement to silence PY3 warnings

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=[
         "requests>=2.22.0,<3.0.0",
         "factory-boy>=2.7.0,<3.0.0",
-        "schematics<=2.1.0,<3.0.0",
+        "schematics>=2.1.1,<3.0.0",
     ],
     extras_require={"dev": ["black", "pytest", "responses"]},
     entry_points={"console_scripts": ["wistia = wistia.cli:main"]},
